6b3c8655f04895a9b55497bbd168276157f472de
GiorgiRukhadze <giorgirukhadze888@gmail.com>
2023-04-06 23:07:31 +0400

    first atm

---
+++ src/components/Products.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+export default function Products({ setCart, cart }) {
+  const [products] = useState([
+    {
+      name: "AA Battery",
+      cost: 2.99,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5-QAul_NfAs-s0XW9M087xWyPOGWvbfYjmqSl0QXabZRSYoid47i7kISiAteyIh0YOci5mtQ&usqp=CAc",
+    },
+    {
+      name: "Blanket",
+      cost: 19.99,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSpwdYDmUL_ZEqhLV7ZWRdQAU7DGcGaxtCt7SrTlL9umrQs2Un7rj9Nbb9Vq01RtEfA0eAVmdt-&usqp=CAc",
+    },
+  ]);
+
+  const addToCart = (product) => {
+    let newCart = [...cart];
+    let itemInCart = newCart.find((item) => product.name === item.name);
+    if (itemInCart) {
+      itemInCart.quantity++;
+    } else {
+      itemInCart = {
+        ...product,
+        quantity: 1,
+      };
+      newCart.push(itemInCart);
+    }
+    setCart(newCart);
+  };
+
+  return (
+    <>
+      <h1>Products</h1>
+      <div className="products">
+        {products.map((product, idx) => (
+          <div className="product" key={idx}>
+            <h3>{product.name}</h3>
+            <h4>${product.cost}</h4>
+            <img src={product.image} alt={product.name} />
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
